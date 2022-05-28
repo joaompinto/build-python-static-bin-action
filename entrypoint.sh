2a5f953120dd06e741248acb6695bf2ff631098c
João Pinto <lamego.pinto@gmail.com>
2022-05-28 18:36:11 +0200

    use tmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export NUITKA_CACHE_DIR=/tmp/cache
 
-nuitka3  --output-dir=/tmp/static \
+nuitka3  \
     --disable-ccache \
     --python-flag=no_site --python-flag=no_warnings \
     --show-progress \
