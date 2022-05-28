60b49c0b67c4156da3450d9716eed9dab871c283
João Pinto <lamego.pinto@gmail.com>
2022-05-28 22:32:33 +0200

    test with custom entry script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pwd
 ls -ltr *
-ls -ltr requiremements.txt
+ls -ltr requirements.txt
 [[ -f requirements.txt]] && pip install --no-cache -r requirements.txt
 exit 1
 nuitka3 --python-flag=no_site \
