2c22fa5d5bc67f605509cf951e4f00a0ea61d4fc
João Pinto <lamego.pinto@gmail.com>
2022-05-28 18:16:53 +0200

    use tmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-nuitka3  --output-dir=/tmp/static --python-flag=no_site --python-flag=no_warnings --show-progress --standalone --follow-imports $1
+nuitka3  --output-dir=/tmp/static \
+    --disable-ccache --disable-bytecode-cache \
+    --python-flag=no_site --python-flag=no_warnings \
+    --show-progress \
+    --standalone --follow-imports $1
 
